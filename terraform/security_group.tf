bfab2f45f5abe3ef68df20cc83f90ccf5c05134f
rashadahmed424 <99238868+rashadahmed424@users.noreply.github.com>
2024-09-27 10:16:11 +0300

    Update security_group.tf

--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -9,6 +9,12 @@ resource "aws_security_group" "my_security_group" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"] # Allow SSH from any IP address
   }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow HTTP traffic from any IP address to jenkins container 
+  }
 
   egress {
     from_port   = 0
